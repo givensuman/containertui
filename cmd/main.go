eaa25bf3ebbfe565b7c2fed78ca0040d130cc1bd
givensuman <givensuman@duck.com>
2025-12-28 16:03:28 -0500

    Sample TUI

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"log"
-
-	tea "github.com/charmbracelet/bubbletea"
-
-	_ "github.com/givensuman/oci-tui/internal/client"
-	"github.com/givensuman/oci-tui/internal/ui"
-)
+import "github.com/givensuman/oci-tui/internal/ui"
 
 func main() {
-	p := tea.NewProgram(ui.NewModel(), tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		log.Fatal(err)
-	}
+	ui.Start()
 }
--- internal/client/client.go
+++ internal/client/client.go
@@ -1,24 +1,112 @@
-// Package client defines means of communicating
-// with the OCI runtime
+// Package client exposes a Docker client wrapper for managing containers.
 package client
 
 import (
-	"log"
+	"context"
 
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
 
-type Client struct {
-	Client *client.Client
+// Container represents a Docker container with essential details.
+type Container struct {
+	container.ContainerJSONBase
+	ID    string `json:"Id"`
+	Name  string `json:"Name"`
+	Image string `json:"Image"`
+	State string `json:"State"`
 }
 
-func NewClient() (*Client, error) {
-	var err error
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		log.Fatalf("failed to create client")
-		return nil, err
-	}
+// ClientWrapper wraps the Docker client to provide container management functionalities.
+type ClientWrapper struct {
+	client *client.Client
+}
 
-	return &Client{Client: cli}, nil
+// NewClient initializes a new Docker client.
+func (cw *ClientWrapper) NewClient() {
+	var err error
+	cw.client, err = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+// CloseClient closes the Docker client connection.
+func (cw *ClientWrapper) CloseClient() {
+	cw.client.Close()
+}
+
+// GetContainers retrieves a list of all Docker containers.
+func (cw *ClientWrapper) GetContainers() []Container {
+	containers, err := cw.client.ContainerList(
+		context.Background(),
+		container.ListOptions{All: true},
+	)
+	if err != nil {
+		panic(err)
+	}
+	var dockerContainers []Container
+	for _, container := range containers {
+		dockerContainers = append(dockerContainers, Container{
+			ID:    container.ID,
+			Name:  container.Names[0][1:],
+			Image: container.Image,
+			State: container.State,
+		})
+	}
+	return dockerContainers
+}
+
+// GetContainerState retrieves the current state of a specific Docker container by its ID.
+func (cw *ClientWrapper) GetContainerState(id string) string {
+	container, _ := cw.client.ContainerInspect(context.Background(), id)
+	return container.State.Status
+}
+
+// PauseContainer pauses a specific Docker container by its ID.
+func (cw *ClientWrapper) PauseContainer(id string) {
+	cw.client.ContainerPause(context.Background(), id)
+}
+
+// PauseContainers pauses multiple Docker containers by their IDs.
+func (cw *ClientWrapper) PauseContainers(ids []string) {
+	for _, id := range ids {
+		cw.PauseContainer(id)
+	}
+}
+
+// UnpauseContainer unpauses a specific Docker container by its ID.
+func (cw *ClientWrapper) UnpauseContainer(id string) {
+	cw.client.ContainerUnpause(context.Background(), id)
+}
+
+// UnpauseContainers unpauses multiple Docker containers by their IDs.
+func (cw *ClientWrapper) UnpauseContainers(ids []string) {
+	for _, id := range ids {
+		cw.UnpauseContainer(id)
+	}
+}
+
+// StartContainer starts a specific Docker container by its ID.
+func (cw *ClientWrapper) StartContainer(id string) {
+	cw.client.ContainerStart(context.Background(), id, container.StartOptions{})
+}
+
+// StartContainers starts multiple Docker containers by their IDs.
+func (cw *ClientWrapper) StartContainers(ids []string) {
+	for _, id := range ids {
+		cw.StartContainer(id)
+	}
+}
+
+// StopContainer stops a specific Docker container by its ID.
+func (cw *ClientWrapper) StopContainer(id string) {
+	cw.client.ContainerStop(context.Background(), id, container.StopOptions{})
+}
+
+// StopContainers stops multiple Docker containers by their IDs.
+func (cw *ClientWrapper) StopContainers(ids []string) {
+	for _, id := range ids {
+		cw.StopContainer(id)
+	}
 }
